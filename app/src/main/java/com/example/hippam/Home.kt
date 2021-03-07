08a0129ec06215388ffb7f7b6b6026c1f641a84c
hanisyah <hanisyahadi4@gmail.com>
2021-03-07 07:46:16 +0700

    commit

--- app/src/main/java/com/example/hippam/Home.kt
+++ app/src/main/java/com/example/hippam/Home.kt
@@ -9,16 +9,11 @@ import android.widget.TextView
 
 class Home : AppCompatActivity() {
     private lateinit var imageIn: ImageView
-    private lateinit var textIn: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         imageIn = findViewById(R.id.imgInput)
         imageIn.setOnClickListener{
-            startActivity(Intent(this, Scan::class.java))
-        }
-        textIn = findViewById(R.id.txtInput)
-        textIn.setOnClickListener{
             startActivity(Intent(this, Scan::class.java))
         }
     }
--- app/src/main/java/com/example/hippam/Scan.kt
+++ app/src/main/java/com/example/hippam/Scan.kt
@@ -1,92 +1,128 @@
 package com.example.hippam
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseArray
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.util.isEmpty
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 
-class Scan : AppCompatActivity(), ZXingScannerView.ResultHandler, View.OnClickListener {
-    private lateinit var keluar: View
-    private lateinit var btnreset: Button
-    private lateinit var text_view_qr_code_value: TextView
-    private lateinit var frame: FrameLayout
-    private lateinit var mScannerView: ZXingScannerView
-    private var isCaptured = false
+class Scan : AppCompatActivity() {
+    private val requestCodeCameraPermission = 1001
+    private lateinit var CameraSurfaceView: SurfaceView
+    private lateinit var textScanResult: TextView
+    private lateinit var cameraSource: CameraSource
+    private lateinit var detector: BarcodeDetector
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-        keluar = findViewById(R.id.back)
-        keluar.setOnClickListener {
-            onBackPressed()
-        }
-        initScannerView()
-        initDefaultView()
-        btnreset = findViewById(R.id.button_reset)
-        btnreset.setOnClickListener(this)
-    }
-    private fun initScannerView() {
-        mScannerView = ZXingScannerView(this)
-        mScannerView.setAutoFocus(true)
-        mScannerView.setResultHandler(this)
-        frame = findViewById(R.id.frame_layout_camera)
-        frame.addView(mScannerView)
-    }
-
-    override fun onStart() {
-        mScannerView.startCamera()
-        doRequestPermission()
-        super.onStart()
-    }
-    private fun doRequestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
-            }
+        CameraSurfaceView = findViewById(R.id.CameraSurfaceView)
+        textScanResult = findViewById(R.id.textScanResult)
+        if (ContextCompat.checkSelfPermission(
+                this@Scan,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            askForCameraPermission()
+        } else {
+            setupControls()
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            100 -> {
-                initScannerView()
+
+    private fun setupControls() {
+        detector = BarcodeDetector.Builder(this@Scan).build()
+        cameraSource = CameraSource.Builder(this@Scan, detector)
+            .setAutoFocusEnabled(true)
+            .build()
+        CameraSurfaceView.holder.addCallback(surgaceCallBack)
+        detector.setProcessor(processor)
+    }
+
+    private fun askForCameraPermission(){
+        ActivityCompat.requestPermissions(
+            this@Scan,
+            arrayOf(android.Manifest.permission.CAMERA),
+            requestCodeCameraPermission)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == requestCodeCameraPermission && grantResults.isEmpty())
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                setupControls()
+            } else{
+                Toast.makeText(applicationContext, "Permission Danied", Toast.LENGTH_SHORT).show()
             }
-            else -> {
-                /* nothing to do in here */
+    }
+
+    private val surgaceCallBack = object : SurfaceHolder.Callback {
+
+        override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun surfaceDestroyed(p0: SurfaceHolder) {
+            cameraSource.stop()
+        }
+
+        @SuppressLint("MissingPermission")
+        override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
+            try {
+                if (ActivityCompat.checkSelfPermission(
+                        this@Scan,
+                        android.Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                cameraSource.start(surfaceHolder)
+            } catch (exception: Exception) {
+                Toast.makeText(applicationContext, "Permission Danied", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    override fun onPause() {
-        mScannerView.stopCamera()
-        super.onPause()
-    }
+    private val processor = object  : Detector.Processor<Barcode>{
+        override fun release() {
 
-    private fun initDefaultView() {
-        btnreset = findViewById(R.id.button_reset)
-        text_view_qr_code_value.text = "QR Code Value"
-        btnreset.visibility = View.GONE
-    }
+        }
 
-    override fun handleResult(rawResult: Result?) {
-        text_view_qr_code_value.text = rawResult?.text
-        btnreset.visibility = View.VISIBLE
-    }
-
-    override fun onClick(view: View?) {
-        when (view?.id) {
-            R.id.button_reset -> {
-                mScannerView.resumeCameraPreview(this)
-                initDefaultView()
-            }
-            else -> {
-
+        override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
+            if (detections != null && detections.detectedItems.isEmpty()){
+                val qrCodes: SparseArray<Barcode> = detections.detectedItems
+                val code = qrCodes.valueAt(0)
+                textScanResult.text = code.displayValue
+            } else{
+                textScanResult.text = " "
             }
         }
     }
